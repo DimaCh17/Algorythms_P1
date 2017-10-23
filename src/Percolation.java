4dce13fe949d909c5055a8b2e6b04d81b2ab92b1
Sergii Tsymbaliuk <ser9ey@gmail.com>
2017-10-23 14:25:53 -0700

    Simple test added

--- src/Percolation.java
+++ src/Percolation.java
@@ -8,6 +8,7 @@ public class Percolation {
     private int start;
     private int end;
     private int[] matrix;
+
     public Percolation(int N) {
     // create N-by-N grid, with all sites blocked
         this.matrix = new int[N*N + 3];
@@ -38,30 +39,38 @@ public class Percolation {
             connect(getPos(i-1, j), position);
         }
         if (i<N-1 && isOpen(i+1, j)) {
-            connect(position, getPos(i+1, j));
+            connect(getPos(i+1, j), position);
         }
         if (j>0 && isOpen(i, j-1)) {
             connect(getPos(i, j-1), position);
         }
         if (j<N-1 && isOpen(i, j+1)) {
-            connect(position, getPos(i, j+1));
+            connect(getPos(i, j+1), position);
         }
+    }
+
+    public int getRoot(int vertex) {
+        while (matrix[vertex] != vertex) {
+            vertex = matrix[vertex];
+        }
+        return vertex;
+    }
+
+    public int getDepth(int vertex) {
+        int depth = 0;
+        while (matrix[vertex] != vertex) {
+            vertex = matrix[vertex];
+            depth++;
+        }
+        return depth;
     }
 
     public void connect(int from, int to) {
         //find parent
-        int rootA = from;
-        int rootB = to;
-        while (matrix[rootA] != rootA && matrix[rootB] != rootB) {
-            rootA = matrix[rootA];
-            rootB = matrix[rootB];
-        }
-        if (matrix[rootA] == rootA) {
-            while (matrix[rootB] != rootB) rootB = matrix[rootB];
-            matrix[rootA] = rootB;
+        if (getDepth(from) > getDepth(to)) {
+            matrix[getRoot(to)] = getRoot(from);
         } else {
-            while (matrix[rootA] != rootA) rootA = matrix[rootA];
-            matrix[rootB] = rootA;
+            matrix[getRoot(from)] = getRoot(to);
         }
     }
 
@@ -73,15 +82,48 @@ public class Percolation {
         return false;
     }
 
+    public boolean isConected(int a, int b) {
+        return getRoot(a) == getRoot(b);
+    }
+
     public boolean percolates()  {           // does the system percolate?
-        return matrix[1] == matrix[N*N+2];
+        return isConected(start, end);
+    }
+
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("Start: ").append(matrix[start]).append("\n");
+        int position = 2;
+        for (int i=0; i<N; i++) {
+            for(int j=0; j<N; j++){
+                sb.append(' ').append(matrix[position]);
+                position++;
+            }
+            sb.append("\n");
+        }
+        sb.append("End: ").append(matrix[end]);
+        return sb.toString();
     }
 
     public static void main(String[] args) { // test client (optional)
-        Scanner in  = new Scanner(System.in);
+//        Scanner in  = new Scanner(System.in);
+//
+//
+//        int N = in.nextInt();
+//        int T = in.nextInt();
 
+        Percolation pc = new Percolation(3);
+        pc.open(0,0);
+        pc.open(1,0);
+        pc.open(2,0);
+        System.out.println(pc);
+        System.out.println("Percolates = " + pc.percolates() + ", expected: True");
 
-        int N = in.nextInt();
-        int T = in.nextInt();
+        pc = new Percolation(3);
+        pc.open(0,0);
+        pc.open(1,1);
+        pc.open(2,2);
+        System.out.println(pc);
+        System.out.println("Percolates = " + pc.percolates() + ", expected: False");
     }
 }
